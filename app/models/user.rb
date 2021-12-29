28cc335a2ceb730d802e23831af6c7a453ab3e95
Salah Alomari <szalomari@gmail.com>
2021-12-29 14:05:33 +0200

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :username, presence: true, 
+                            uniqueness: { case_sensitive: false }, 
+                            length: { in: 3..25 }
+    VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        format: { with: VALID_EMAIL_FORMAT, message: "enter a valid email" }
 end
